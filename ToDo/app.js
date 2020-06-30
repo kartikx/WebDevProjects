af28dab8c4c6808ab906ab0957a557f89aadc967
Kartik Ramesh <kartikx2000@gmail.com>
2020-06-30 10:56:33 +0530

    Polish JS file

--- ToDo/app.js
+++ ToDo/app.js
@@ -17,28 +17,32 @@ function addTodo(event) {
     // Prevent form submission on click
     event.preventDefault();
 
+    // This is the div surrounding each new todo.
     const todoDiv = document.createElement("div");
     todoDiv.classList.add("todo");
 
     // This is the newly created item.
     const todoItem = document.createElement("li");
-    todoItem.innerText = "Take out the Trash";
+    todoItem.innerText = todoInput.value;
     todoItem.classList.add("todo-item");
     todoDiv.appendChild(todoItem);
 
     // Check mark button
     const checkButton = document.createElement("button");
     // This allows us to add HTML text inside, we could have done using an appendChild to checkButton as well.
-    checkButton.innerHTML = '<i class="fas fa-check"></i>';
+    checkButton.innerHTML = '<i class="fas fa-check-square"></i>';
     checkButton.classList.add("check-btn");
     todoDiv.appendChild(checkButton);
 
     // Trash button
     const trashButton = document.createElement("button");
-    trashButton.innerHTML = '<i class="fas fa-trash"></i>';
+    trashButton.innerHTML = '<i class="fas fa-minus-circle"></i>';
     trashButton.classList.add("trash-btn");
     todoDiv.appendChild(trashButton);
 
     // Add div to the UL
     todoList.appendChild(todoDiv);
+
+    // Clear input field
+    todoInput.value = "";
 }
