173002138ee3792e622b6a9885638cb921483fad
Kartik Ramesh <kartikx2000@gmail.com>
2020-06-30 22:01:46 +0530

    polish js

--- Form/app.js
+++ Form/app.js
@@ -5,25 +5,30 @@ function animateForm(){
      */
     const arrows = document.querySelectorAll('.fa-arrow-down');
     arrows.forEach(arrow => {
-        const input = arrow.previousElementSibling;
-        const parent = arrow.parentElement;
-        const nextForm = parent.nextElementSibling;
-
-        // Validate inputs
-        if (input.type === 'text' && validateUser(input.value)){
-            nextSlide(parent, nextForm);
-        } else if (input.type === 'email' && validateEmail(input.value)){
-            nextSlide(parent, nextForm);
-        }
+        arrow.addEventListener("click", () => {
+            const input = arrow.previousElementSibling;
+            const parent = arrow.parentElement;
+            const nextForm = parent.nextElementSibling;
+            console.log('here');
+            // Validate inputs
+            if (input.type === 'text' && validateUser(input.value)){
+                nextSlide(parent, nextForm);
+            } else if (input.type === 'email' && validateEmail(input.value)){
+                nextSlide(parent, nextForm);
+            } else if(input.type === 'password'){
+                nextSlide(parent, nextForm);
+            }
+        });
     });
 }
 
 function validateUser(username){
+    console.log('validating user');
     if (username.length < 3){
-        bg_error(false);
+        bg_error(true);
         return false;
     }else{
-        bg_error(true);
+        bg_error(false);
         return true;
     }
 }
@@ -31,17 +36,20 @@ function validateUser(username){
 function validateEmail(email){
     const validation = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
     if (validation.test(email)){
-        bg_error(true);
+        bg_error(false);
         return true;
     }else{
-        bg_error(false);
+        bg_error(true);
         return false;
     }
 }
 
 function bg_error(error){
     if (error){
-        document.body.classList.toggle('error-bg');
+        // document.body.classList.add('error-bg');
+        document.body.style.backgroundImage = 'linear-gradient(120deg, rgb(172, 5, 5), rgb(61, 2, 2))'
+    }else{
+        document.body.style.backgroundImage = 'linear-gradient(120deg, rgb(34, 185, 34), rgb(32, 80, 212))'
     }
 }
 
@@ -50,3 +58,5 @@ function nextSlide(parent, nextForm){
     parent.classList.remove('active');
     nextForm.classList.add('active');
 }
+
+animateForm();
