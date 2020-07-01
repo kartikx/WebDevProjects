31892d94ae607f1423293b592ac7ebc48eb108f8
Kartik Ramesh <kartikx2000@gmail.com>
2020-07-01 18:31:20 +0530

    Using slots to embed HTML in

--- Vue/Components/modalcomponents.js
+++ Vue/Components/modalcomponents.js
@@ -6,11 +6,16 @@
  */
 
 Vue.component("modal", {
+    /**
+     * Whether to use slots or props depends on your use case,
+     * here we want to embed HTML inside the modal class, hence we
+     * use slot.
+     */
     template: `
     <div class="modal is-active">
         <div class="modal-background"></div>
         <div class="modal-content">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo consectetur ullam perferendis rerum,
+            <slot></slot>
         </div>
         <button class="modal-close is-large" aria-label="close" @click="$emit('close')"></button>
     </div>
