bdc2a939095c7bc227edfe0f19c65509098e00cd
Kartik Ramesh <kartikx2000@gmail.com>
2020-07-02 19:51:57 +0530

    Accessing AJAX request variables in Server Side

--- CS50/CurrencyExchanger/app.py
+++ CS50/CurrencyExchanger/app.py
@@ -6,3 +6,13 @@ app = Flask(__name__)
 @app.route('/')
 def home():
     return render_template('index.html')
+
+'''
+The JS request is sent to this route.
+It should have access to the base and symbol variables as a JSON object.
+'''
+@app.route('/convert', methods=['POST'])
+def convert():
+    base   = request.form["base"]
+    symbol = request.form["symbol"]
+
