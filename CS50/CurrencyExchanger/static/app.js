33054ff02ed2eb02d7c68cb90fad0f391886924f
Kartik Ramesh <kartikx2000@gmail.com>
2020-07-02 19:48:51 +0530

    Definition of optionChange(), called when drop-down boxes value is changed

--- CS50/CurrencyExchanger/static/app.js
+++ CS50/CurrencyExchanger/static/app.js
@@ -7,3 +7,13 @@ document.addEventListener('DOMContentLoaded', () => {
     box1.onchange = optionChange;
     box2.onchange = optionChange;
 });
+
+/**
+ * This function should see the current values of 
+ * the two drop-down boxes, and then call an AJAX
+ * request to the backend.
+ */
+function optionChange() {
+    const request = new XMLHttpRequest();
+    const base = box1.value;
+    const symbol = box2.value;
