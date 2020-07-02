7a51201b0ff0396ad76f4c1de15ae9d1ecb175b8
Kartik Ramesh <kartikx2000@gmail.com>
2020-07-02 20:07:31 +0530

    Remove debug statement

--- CS50/CurrencyExchanger/static/app.js
+++ CS50/CurrencyExchanger/static/app.js
@@ -3,7 +3,6 @@ const box1   = document.querySelector('#box1');
 const box2   = document.querySelector('#box2');
 
 document.addEventListener('DOMContentLoaded', () => {
-    console.log('loaded');
     box1.onchange = optionChange;
     box2.onchange = optionChange;
 });
