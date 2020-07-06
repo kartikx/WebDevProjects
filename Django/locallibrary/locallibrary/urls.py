93e11ff1391ef83dbc09ca7a2a2cf2444d0d9c5d
Kartik Ramesh <kartikx2000@gmail.com>
2020-07-06 16:46:29 +0530

    Polish in Urls

--- Django/locallibrary/locallibrary/urls.py
+++ Django/locallibrary/locallibrary/urls.py
@@ -13,25 +13,26 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
 from django.urls import path, include
 from django.views.generic import RedirectView
 from django.conf import settings
 from django.conf.urls.static import static
 
+
+"""
+1.  Instead of adding all mappings here,
+    it's considered good practice to defer it
+    to the app
+2.  Configuring the root path to display
+    the catalog page.
+"""
+
 urlpatterns = [
     path('admin/', admin.site.urls),
 
-    """
-    Instead of adding all mappings here,
-    it's considered good practice to defer it
-    to the app
-    """
     path('catalog/', include('catalog.urls')),
 
-    """
-    Configuring the root path to display
-    the catalog page.
-    """
-    path('', RedirectView.as_view(url='catalog/', permanent=True)),
+    path('', RedirectView.as_view(url='catalog/')),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
