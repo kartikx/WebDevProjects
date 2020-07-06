05f7c2947d988b701c7f910a3cce133ffde4bb13
Kartik Ramesh <kartikx2000@gmail.com>
2020-07-06 11:09:53 +0530

    URL mapping for Catalog

--- Django/locallibrary/locallibrary/urls.py
+++ Django/locallibrary/locallibrary/urls.py
@@ -14,8 +14,24 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from django.views.generic import RedirectView
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-]
+
+    """
+    Instead of adding all mappings here,
+    it's considered good practice to defer it
+    to the app
+    """
+    path('catalog/', include('catalog.urls')),
+
+    """
+    Configuring the root path to display
+    the catalog page.
+    """
+    path('', RedirectView.as_view(url='catalog/', permanent=True)),
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
