6b77262c845a5357ca111adca44730bc40fe5e19
Kartik Ramesh <kartikx2000@gmail.com>
2020-07-06 13:47:27 +0530

    Book Instance, Author and Language

--- Django/locallibrary/catalog/models.py
+++ Django/locallibrary/catalog/models.py
@@ -41,3 +41,72 @@ class Book(models.Model):
     """
     def __get__absolute__url(self):
         return reverse('book-details', args=[str(self.id)])
+
+
+class BookInstance(models.Model):
+
+    """
+    By default the primary key is set to be an 
+    AutoField, however you can explicitly set
+    using UUIDField.
+    """
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, help_text='Unique ID for this particular book, across the whole library')
+    book = models.ForeignKey(Book, on_delete=models.SET_NULL, null=True)
+    imprint = models.CharField(max_length=200)
+    due_back = models.DateField(null=True, blank=True)
+    
+    LOAN_STATUS = (
+        ('m', 'Maintenance'),
+        ('o', 'On loan'),
+        ('a', 'Available'),
+        ('r', 'Reserved'),
+    )
+
+    status = models.CharField(
+        max_length=1,
+        choices=LOAN_STATUS,
+        blank=True,
+        default='m',
+        help_text='Book availability',
+    )
+
+    class Meta:
+        ordering = ['due_back']
+    
+    def __str__(self):
+        return f('{self.id} ({self.book.title})')
+    
+
+class Author(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+
+    date_of_birth = models.DateField(null=True, blank=True)
+    date_of_death = models.DateField('Died', null=True, blank=True)
+
+    class Meta:
+        ordering = ['first_name', 'last_name']
+
+    def get_absolute_url(self):
+        return reverse('author-details', args=[str(self.id)])
+
+    def __str__(self):
+        return f('{self.first_name},{self.last_name}')
+
+class Language(models.Model):
+    LANGUAGE_LIST = (
+        ('eng', 'English' ),
+        ('fre', 'French'),
+        ('jap', 'Japanese')
+    )
+
+    # Set up choices when working.
+    # choices=LANGUAGE_LIST,
+    # blank=True,
+    # default='eng',
+
+    language = models.CharField(max_length=30,
+                help_text='Book language')
+
+    def __str__(self):
+        return (self.language)
