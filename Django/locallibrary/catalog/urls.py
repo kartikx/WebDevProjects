26858f3c8d2ef9de7795496376fb1a80eb3e61fc
Kartik Ramesh <kartikx2000@gmail.com>
2020-07-07 10:25:11 +0530

    index view

--- Django/locallibrary/catalog/urls.py
+++ Django/locallibrary/catalog/urls.py
@@ -1,6 +1,13 @@
 from django.urls import path
 from . import views
 
+"""
+When this urlpatterns is checked,
+the url has already been matched for catalog/.
+Hence all paths in this are catalog/$
+"""
+
 urlpatterns = [
-    
+    path('', views.index, name='index'),
+    path('books', BookListView.as_view(), name='books')
 ]
